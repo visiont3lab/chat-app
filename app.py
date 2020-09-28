30682ded9252553c0a6a84568736e2efd3f31277
manuelrucci <rucci@localhost.com>
2020-09-28 09:19:26 +0200

    upaded style and added room

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+rooms = ["lesson","entertainment","news"]
+
 @socketio.on('connected')
 def handle_connected(json, methods=['GET', 'POST']):
     #print("Risultati: ", json["data"]["m"])
@@ -23,16 +25,42 @@ def on_message(data):
     """Broadcast messages"""
     msg = data["msg"]
     username = data["username"]
-    #room = data["room"]
+    room = data["room"]
     # Set timestamp
     time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
-    socketio.emit('message', {"username": username, "msg": msg, "time_stamp": time_stamp})
+    #socketio.emit('message', {"username": username, "msg": msg, "time_stamp": time_stamp})
     #socketio.emit('message', {"msg": msg, "time_stamp": time_stamp})  
-    #send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=room)
+    send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=room)
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def sessions():
-    return render_template('index.html')
+    return render_template('index.html', rooms=rooms)
+
+@socketio.on('join')
+def on_join(data):
+    """User joins a room"""
+    username = data["username"]
+    room = data["room"]
+    for r in rooms:
+        if r==room:
+            join_room(r)
+            time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
+            msg =  username + " has joined the " + r + " room."
+            send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=r)
+        else:
+            leave_room(r)
+            time_stamp = time.strftime('%b-%d %I:%M%p', time.localtime())
+            msg =  username + " has left the " + r + " room."
+            send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=r)
+
+@socketio.on('leave')
+def on_leave(data):
+    """User leaves a room"""
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send({"msg": username + " has left the room"}, room=room)
+
 
 if __name__ == "__main__":
     app.run(debug=False)
